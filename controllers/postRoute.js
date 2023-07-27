2d039cd84739098e199218692a618759e5f33b3c
KEINance <keinance2@gmail.com>
2023-07-26 20:37:54 -0400

    postroute syntax error corrected

--- controllers/postRoute.js
+++ controllers/postRoute.js
@@ -21,7 +21,7 @@ router.post('/', async (req, res) => {
 
     res.status(200)json.(newItemCreated);
     } catch (err) {
-        res.status(500).json.(err);
+        res.status(500).json(err);
     }
 });
 
