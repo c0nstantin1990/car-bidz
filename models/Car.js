8672d1ab68061bbc86d3ae68672166ffa1dd3a05
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-07-31 21:31:17 -0400

    fixing errors

--- models/Car.js
+++ models/Car.js
@@ -1,54 +1,54 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Car extends Model { }
+class Car extends Model {}
 
 Car.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        make: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        model: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        year: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        price: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        start_bid: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-        },
-        image_url: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "user",
-                key: "id",
-            },
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: "car",
-    }
+    make: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    start_bid: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "car",
+  }
 );
 
 module.exports = Car;
--- server.js
+++ server.js
@@ -1,46 +1,39 @@
 const path = require("path");
+require("dotenv").config();
 const express = require("express");
-const session = require("express-session");
+const routes = require("./controllers/");
+const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
-const routes = require("./controllers");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const helpers = require("./utils/helpers");
 
-const sequelize = require("./config/connection.js");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: "Super secret secret",
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: "strict",
-  },
+  cookie: { maxAge: 3600000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
   }),
 };
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(session(sess));
+app.use(express.static(path.join(__dirname, "public")));
 
-// Inform Express.js on which template engine to use
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(session(sess));
 
 app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
