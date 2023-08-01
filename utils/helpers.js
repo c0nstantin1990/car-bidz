604932697a5267de863f3729d4a93390f7599216
KEINance <keinance2@gmail.com>
2023-07-31 20:07:25 -0400

    high bid help

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,5 +5,14 @@ module.exports = {
       formattedDate.getMonth() + 1
     }/${formattedDate.getDate()}/${formattedDate.getFullYear()}`;
   },
+  highest_bid: (bids) => {
+    const bidArray = [];
+    for (var i = 0; i < bids.length; i++) {
+      bidArray.push(bids[i].bid_price);
+    }
+    console.log(bidArray);
+    const greatestBid = Math.max(...bidArray)
+    return `${greatestBid}`
+  }
 };
 
